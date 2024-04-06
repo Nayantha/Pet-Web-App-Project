11ab6c1eaa982c83858b0d9fba479cf79fe96ce9
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-04-06 17:40:37 +0530

    initialize the server and add health endpoint.

--- backend/rust/backend/src/main.rs
+++ backend/rust/backend/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/health")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let ip_address = "127.0.0.1";
+    let port = 8080;
+    let server_url = format!("http://{}:{}", ip_address, port);
+
+    println!("Server is initializing...");
+    println!("Server URL: {}", server_url);
+
+    let server = HttpServer::new(|| {
+        App::new()
+            .service(health_check)
+    })
+    .bind(format!("{}:{}", ip_address, port))?
+    .run();
+
+    println!("Server started at {}", server_url);
+
+    server.await
 }
