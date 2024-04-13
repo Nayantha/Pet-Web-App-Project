c2ef9ecc7068baddb4164081a4f5fc74191045b8
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-04-13 13:28:24 +0530

    remove rust backend.

--- backend/rust/backend/src/models/api_response.rs
+++
@@ -1,8 +0,0 @@
-use serde::Serialize;
-
-#[derive(Debug, Serialize)]
-pub struct ApiResponse {
-    pub success: bool,
-    pub data: Option<serde_json::Value>,
-    pub error: Option<String>,
-}
--- backend/rust/backend/src/models/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod user;
-pub mod api_response;
-pub mod auth_error;
