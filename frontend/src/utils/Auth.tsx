171bab63d9fba8e878a8b5efdffbbd0613b98786
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-05-19 13:24:14 +0530

    add util function to handle auth.

---
+++ frontend/src/utils/Auth.tsx
@@ -0,0 +1,22 @@
+import pb from "lib/pocketbase"
+import { UserRegisterData } from "models/UserRegisterData.ts";
+
+export async function login(data: UserRegisterData) {
+    await loginToPB( data );
+}
+
+async function loginToPB(data: UserRegisterData) {
+    try {
+        await
+            pb.collection( import.meta.env.VITE_PB_USER_TABLE )
+                .authWithPassword( data.email, data.password );
+        window.location.href = "/";
+    } catch (e) {
+        alert( e )
+    }
+}
+
+export function logout() {
+    pb.authStore.clear();
+    window.location.reload();
+}
