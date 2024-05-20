32e4653459ed56da296f9aec57125e4d9f0eb126
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-05-20 14:04:38 +0530

    replace the existing components with the profile component.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,24 +1,11 @@
-import { Link } from "react-router-dom";
-import pb from "lib/pocketbase.ts";
-import { logout } from "utils/Auth.tsx";
+import Profile from "components/Profile.tsx";
 
 function Home() {
-    const isLoggedIn = pb.authStore.isValid;
-
-    if ( isLoggedIn )
-        return (
-            <>
-                <h1>Logged in: {pb.authStore.isValid.toString()}</h1>
-                <button onClick={logout}>Logout</button>
-            </>
-        )
 
     return (
         <>
             <div>Home</div>
-            <Link to="/login">Login</Link>
-            <br/>
-            <Link to="/register">Register</Link>
+            <Profile/>
         </>
     )
 }
