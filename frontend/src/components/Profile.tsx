e3bac7493dc5eba4eb31b15327028e532124df75
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-05-20 18:43:21 +0530

    replace the custom html with chakra ui widgets.

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -2,27 +2,80 @@ import pb from "lib/pocketbase.ts";
 import { logout } from "../utils/Auth.tsx";
 import { Link } from "react-router-dom";
 import { RecordModel } from "pocketbase";
+import {
+    Button,
+    Drawer,
+    DrawerBody,
+    DrawerContent,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    useDisclosure
+} from '@chakra-ui/react'
+import { useRef } from "react";
 
-function Profile(){
+function Profile() {
     const isLoggedIn = pb.authStore.isValid;
     const userModel: RecordModel = pb.authStore.model as RecordModel;
-
+    const {isOpen, onOpen, onClose} = useDisclosure()
+    const btnRef = useRef()
     if ( isLoggedIn )
         return (
-            <div>
-                <img src={pb.files.getUrl(userModel, userModel.avatar)} alt={"profile pic of user" + userModel.username}/>
-                <div>Email : {userModel?.email}</div>
-                <div>Name : {userModel?.name}</div>
-                <div>UserName : {userModel?.username}</div>
-                <button onClick={logout}>Logout</button>
-            </div>
+            <>
+                <Button ref={btnRef} colorScheme='teal' onClick={onOpen}>
+                    Open
+                </Button>
+                <Drawer
+                    isOpen={isOpen}
+                    placement='right'
+                    onClose={onClose}
+                    finalFocusRef={btnRef}
+                >
+                    <DrawerOverlay/>
+                    <DrawerContent>
+                        <DrawerHeader>
+                            <img src={pb.files.getUrl( userModel, userModel.avatar )}
+                                 alt={"profile pic of user" + userModel.username}/>
+                        </DrawerHeader>
+                        <DrawerBody>
+                            <div>Email : {userModel?.email}</div>
+                            <div>Name : {userModel?.name}</div>
+                            <div>UserName : {userModel?.username}</div>
+                        </DrawerBody>
+                        <DrawerFooter>
+                            <Button variant='outline' mr={3} onClick={onClose}>
+                                Close
+                            </Button>
+                            <Button colorScheme='blue' onClick={logout}>Logout</Button>
+                        </DrawerFooter>
+                    </DrawerContent>
+                </Drawer>
+            </>
         )
 
     return (
         <>
-            <Link to="/login">Login</Link>
-            <br/>
-            <Link to="/register">Register</Link>
+            <Button ref={btnRef} colorScheme='teal' onClick={onOpen}>
+                Open
+            </Button>
+            <Drawer
+                isOpen={isOpen}
+                placement='right'
+                onClose={onClose}
+                finalFocusRef={btnRef}
+            >
+                <DrawerOverlay/>
+                <DrawerContent>
+                    <DrawerHeader>
+                        Login or Register
+                    </DrawerHeader>
+                    <DrawerBody>
+                        <Link to="/login">Login</Link>
+                        <br/>
+                        <Link to="/register">Register</Link>
+                    </DrawerBody>
+                </DrawerContent>
+            </Drawer>
         </>
     )
 }
