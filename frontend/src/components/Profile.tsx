c71d94a28e47fe25d2de1cc182f34c88ac6ed88d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-05-20 14:02:50 +0530

    add user profile component.

---
+++ frontend/src/components/Profile.tsx
@@ -0,0 +1,31 @@
+import pb from "lib/pocketbase.ts";
+import { logout } from "../utils/Auth.tsx";
+import { Link } from "react-router-dom";
+import { RecordModel } from "pocketbase";
+
+function Profile(){
+    const isLoggedIn = pb.authStore.isValid;
+    const userModel: RecordModel = pb.authStore.model as RecordModel;
+
+    if ( isLoggedIn )
+        return (
+            <div>
+                <img src={pb.files.getUrl(userModel, userModel.avatar)} alt={"profile pic of user" + userModel.username}/>
+                <div>Avatar : {userModel?.avatar}</div>
+                <div>Email : {userModel?.email}</div>
+                <div>Name : {userModel?.name}</div>
+                <div>UserName : {userModel?.username}</div>
+                <button onClick={logout}>Logout</button>
+            </div>
+        )
+
+    return (
+        <>
+            <Link to="/login">Login</Link>
+            <br/>
+            <Link to="/register">Register</Link>
+        </>
+    )
+}
+
+export default Profile;
