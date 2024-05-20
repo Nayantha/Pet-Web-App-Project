cfaa15e0c3b5c6beaab209be506956abbd694782
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-05-20 12:59:11 +0530

    remove zod validation.

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -3,38 +3,10 @@ import { useState } from "react";
 import { registerUser } from "utils/Auth.tsx";
 import { UserRegisterData } from "models/UserRegisterData.ts";
 import { Link } from "react-router-dom";
-import { Grid, TextField, Typography} from "@mui/material";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { Grid, TextField, Typography } from "@mui/material";
 
 function Register() {
-    const userRegisterSchema = z.object({
-        email: z.string().email("Invalid email").min(1, "Email is required."),
-        name: z.string().min(6, "Name is required."),
-        username: z.string().min(6, "UserName is required.").max(25),
-        password: z.string().min(8, "Password must be at least 6 characters of length.").max(16),
-        passwordConfirm: z.string().min(8, "Password must be at least 6 characters of length.").max(16)
-    });
-
-    const {register, handleSubmit, formState: {errors, touchedFields}} = useForm<UserRegisterData>({
-        resolver: zodResolver(userRegisterSchema),
-        defaultValues: {
-            email: "",
-            password: "",
-            name: "",
-            username: "",
-            passwordConfirm: ""
-        }
-    });
-    // @ts-ignore
-    const defaultValues: UserRegisterData = {
-        avatar: undefined,
-        email: "",
-        password: "",
-        name: "",
-        username: "",
-        passwordConfirm: ""
-    }
+    const {register, handleSubmit, formState: {errors}} = useForm();
     const [isLoading, setLoading] = useState( false );
 
     async function handleRegister(data: any) {
@@ -42,7 +14,7 @@ function Register() {
         setLoading( true );
         await registerUser( data as UserRegisterData );
         setLoading( false );
-    };
+    }
 
     return (
         <>
@@ -51,37 +23,32 @@ function Register() {
             <form onSubmit={handleSubmit( handleRegister )}>
                 <Grid container spacing={2}>
                     <Grid item xs={6}>
-                            <TextField label="name" margin="normal" type="text"
-                                       {...register( "name", {required: true} )} error={!!errors.name}
-                                       helperText={errors.name?.message} defaultValue={defaultValues.name}/>
+                        <TextField label="name" margin="normal" type="text"
+                                   {...register( "name", {required: true} )} error={!!errors.name}/>
                     </Grid>
                     <Grid item xs={6}>
                         <TextField label="username" margin="normal"
-                                   type="text" {...register( "username", {required: true} )}
-                                   helperText={errors.username?.message} defaultValue={defaultValues.username}/>
+                                   type="text" {...register( "username", {required: true} )}/>
                     </Grid>
                     <Grid item xs={6}>
                         <TextField label="email" margin="normal"
                                    type="email"  {...register( "email", {
                             required: true,
                             pattern: /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim
-                        } )}
-                                   helperText={errors.email?.message} defaultValue={defaultValues.email}/>
+                        } )}/>
                     </Grid>
                     <Grid item xs={6}>
                         <TextField label="password" margin="normal" type="password" {...register( "password", {
                             required: true,
                             pattern: /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{8,16})\S$/gim
-                        } )}
-                                   helperText={errors.password?.message} defaultValue={defaultValues.password}/>
+                        } )}/>
                     </Grid>
                     <Grid item xs={6}>
                         <TextField label="retype password" margin="normal"
                                    type="password" {...register( "passwordConfirm", {
                             required: true,
                             pattern: /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{8,16})\S$/gim
-                        } )}
-                                   helperText={errors.passwordConfirm?.message} defaultValue={defaultValues.passwordConfirm}/>
+                        } )}/>
                     </Grid>
                     <Grid item xs={12}>
                         <input type="file"
