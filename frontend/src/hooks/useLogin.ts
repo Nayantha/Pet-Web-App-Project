17000df73be7a31af9808d076e1444ff68090687
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-05-28 13:47:30 +0530

    refactor code with use of the React query.

--- frontend/src/hooks/useLogin.ts
+++ frontend/src/hooks/useLogin.ts
@@ -1,22 +1,15 @@
 import { UserLoginData } from "models/UserLoginData.ts";
 import pb from "../lib/pocketbase.ts";
-import { useState } from "react";
+import { useMutation } from "react-query";
 
 export default function useLogin() {
-    const [isLoading, setLoading] = useState(false);
 
     async function login(data: UserLoginData) {
-        setLoading(true);
-        try {
-            await
-                pb.collection(import.meta.env.VITE_PB_USER_TABLE)
-                    .authWithPassword(data.email, data.password);
-            window.location.href = "/";
-        } catch (e) {
-            alert(e)
-        }
-        setLoading(false);
+        await
+            pb.collection(import.meta.env.VITE_PB_USER_TABLE)
+                .authWithPassword(data.email, data.password);
+        window.location.href = "/";
     }
 
-    return { login, isLoading };
+    return useMutation(login);
 }
