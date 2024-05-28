ac79cdbbfad7d93c7dee3cd67e0b8d7ad5d083d9
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-05-28 21:53:58 +0530

    use react query to fetch data.

--- frontend/src/pages/PetPage.tsx
+++ frontend/src/pages/PetPage.tsx
@@ -1,39 +1,34 @@
 import pb from "lib/pocketbase.ts";
-
-import { useEffect, useState } from "react";
 import { Pet as PetInterface } from "../models/Pet.ts";
 import PetComponent from "../components/PetComponent.tsx";
 import { useParams } from "react-router-dom";
+import { useQuery } from "react-query";
 
 export default function PetPage() {
     const { id } = useParams();
-    const [pet, setPet] = useState<PetInterface>({
-        adopted: false,
-        baseColor: "",
-        breed: "",
-        gender: "",
-        id: "",
-        intakeDate: "",
-        intakeReason: "",
-        species: "",
-        name: ""
-    });
 
-    async function getPet() {
-        const record = await pb.collection(import.meta.env.VITE_PB_PET_TABLE).getOne(id);
-        // @ts-ignore
-        const pet = record as PetInterface;
-        setPet(pet);
+    const { data, isLoading, isError, error } = useQuery(['post', id], () => fetchPet(id));
+
+    async function fetchPet(id: string | undefined) {
+
+        if (id != null) {
+            const record = await pb.collection(import.meta.env.VITE_PB_PET_TABLE).getOne(id);
+            // @ts-ignore
+            return record as PetInterface;
+        } else {
+            window.location.href = "/";
+        }
+
     }
 
-    useEffect(() => {
-        getPet();
-    }, [])
+    if (isLoading) return <div>Loading post...</div>;
+    if (isError) { // @ts-ignore
+        return <div>Error: { error.message }</div>;
+    }
 
     return (
         <>
-            <p>{ id }</p>
-            <PetComponent pet={ pet }/>
+            <PetComponent pet={ data as PetInterface }/>
         </>
     )
 }
