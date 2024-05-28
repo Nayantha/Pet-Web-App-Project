3df4b0a961dc2a343f7bab86e81b970be1fa8955
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-05-28 19:15:04 +0530

    add next, prev buttons and generate pagination links.

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -1,12 +1,37 @@
-import { Button, Flex } from "@chakra-ui/react";
+import { Button, Flex, IconButton } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
 
 export default function Pagination({ metadata }: { metadata: ListMetadata }) {
+    const pageNumbers = Array.from({ length: metadata.totalPages }, (_, index) => index + 1);
     return (
-        <Flex>
-            <Link to="/pets"><Button>1</Button></Link>
-            <Link to="/pets/?p=2"><Button>2</Button></Link>
-        </Flex>
+        <>
+            <Flex gap={ 2 }>
+
+                { metadata.page > 1 && (
+                    <Link to={ `/pets/?p=${ metadata.page - 1 }` }>
+                        <IconButton aria-label="Previous Page" icon={ <ArrowBackIcon/> } mr={ 2 }/>
+                    </Link>
+                ) }
+
+                { pageNumbers.map((no) => {
+                        const isFirstPage = no === 1;
+                        const isCurrentPage = no === metadata.page;
+                        return (
+                            <Link key={ no } to={ isFirstPage ? "/pets" : `/pets/?p=${ no }` }
+                                  className={ isCurrentPage ? "disabled" : "" }><Button>{ no }</Button></Link>
+                        )
+                    }
+                ) }
+
+                { metadata.page < metadata.totalPages && (
+                    <Link to={ `/pets/?p=${ metadata.page + 1 }` }>
+                        <IconButton aria-label="Next Page" icon={ <ArrowForwardIcon/> } ml={ 2 }/>
+                    </Link>
+                ) }
+
+            </Flex>
+        </>
     );
 }
 
