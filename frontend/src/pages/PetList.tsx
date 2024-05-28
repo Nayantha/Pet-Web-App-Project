8ed5b467c48be3094025c5ddd1d7150c879f928a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-05-28 18:31:28 +0530

    extract metadata from api call and pass to component.

--- frontend/src/pages/PetList.tsx
+++ frontend/src/pages/PetList.tsx
@@ -3,12 +3,54 @@ import pb from "lib/pocketbase.ts";
 import { useEffect, useState } from "react";
 import { Pet as PetInterface } from "../models/Pet.ts";
 import PetComponent from "../components/PetComponent.tsx";
+import Pagination from "../components/Pagination.tsx";
+import { useLocation } from "react-router-dom";
+import { ListResult, RecordModel } from "pocketbase";
 
 export default function PetList() {
     const [pets, setPets] = useState<PetInterface[]>([]);
+    const [metadata, setMetadata] = useState<ListMetadata>({
+        page: 0,
+        perPage: 0,
+        totalItems: 0,
+        totalPages: 0
+    });
+    const location = useLocation();
+
+    function setMetadataIfEmpty(resultList: ListResult<RecordModel>) {
+        const isResultListEmpty =
+            resultList &&
+            Object.values(resultList).every((value) => value === 0);
+
+        if (isResultListEmpty) {
+            // Set default metadata if resultList is empty or all properties are 0
+            setMetadata({ page: 0, perPage: 0, totalItems: 0, totalPages: 0 } as ListMetadata);
+        } else {
+            setMetadata(resultList as ListMetadata);
+        }
+    }
 
     async function getPets() {
-        const resultList = await pb.collection(import.meta.env.VITE_PB_PET_TABLE).getList(1, import.meta.env.VITE_PB_PET_LIST_SIZE);
+
+        const queryParams = new URLSearchParams(location.search);
+        let page: number;
+
+        const pageParam = queryParams.get('p');
+        if (pageParam !== null && pageParam !== undefined) {
+            page = +pageParam || 1;
+        } else {
+            page = 1;
+        }
+
+        // Check if page is less than or equal to zero, or larger than totalPages
+        if (page <= 0 || page > metadata.totalPages) {
+            page = 1; // Set page to one if it's invalid
+        }
+
+        const resultList = await pb.collection(import.meta.env.VITE_PB_PET_TABLE).getList(page, import.meta.env.VITE_PB_PET_LIST_SIZE);
+
+        // using this we can only set the metadata values once and use on all pages.
+        setMetadataIfEmpty(resultList);
 
         const petList = resultList.items.map(function (pet) {
             // @ts-ignore
@@ -20,13 +62,14 @@ export default function PetList() {
 
     useEffect(() => {
         getPets();
-    }, [])
+    }, [location.search])
 
     return (
         <>
-            {pets.map((pet) => (
+            <Pagination metadata={ metadata }/>
+            { pets.map((pet) => (
                 <PetComponent key={ pet.id } pet={ pet }/>
-            ))}
+            )) }
         </>
     )
 }
