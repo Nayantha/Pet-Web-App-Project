b7812e746f2794941648f9eb5fc6f07e90d1706c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-05-28 20:19:43 +0530

    add a dynamic path to pets.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import 'App.css';
-import {BrowserRouter as Router, Navigate, Route, Routes,} from "react-router-dom";
+import { BrowserRouter as Router, Navigate, Route, Routes, } from "react-router-dom";
 import Home from "pages/Home.tsx";
 import Login from "pages/Login.tsx";
 import Register from "pages/Register.tsx";
@@ -14,9 +14,9 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home/>}/>
                     <Route path="/login" element={<Login/>}/>
-                    <Route path="/register" element={<Register/>}/>
-                    <Route path="/pet" element={<PetPage/>}/>
-                    <Route path="/pets" element={<PetList/>}/>
+                    <Route path="/register" element={ <Register/> }/>
+                    <Route path="/pets/:id" element={ <PetPage/> }/>
+                    <Route path="/pets" element={ <PetList/> }/>
                     <Route path="*" element={<Navigate to="/"/>}/>
                 </Routes>
             </Router>
