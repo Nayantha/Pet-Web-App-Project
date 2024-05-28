49cf02b1509c1fff0827a3addedbe5fa2b42d187
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-05-28 20:08:21 +0530

    use the theme.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
@@ -10,7 +10,8 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <QueryClientProvider client={ queryClient }>
-            <ChakraProvider>
+            <ChakraProvider theme={ theme }>
+                <ColorModeScript initialColorMode={ theme.config.initialColorMode }/>
                 <App/>
             </ChakraProvider>
         </QueryClientProvider>
