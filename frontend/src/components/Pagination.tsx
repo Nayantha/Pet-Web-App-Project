495e623a138d829bbfc5ca764c235d6a06c4143e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-05-28 20:04:57 +0530

    wrap the flex component with box.

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, IconButton } from "@chakra-ui/react";
+import { Box, Button, Flex, IconButton } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
 
@@ -6,31 +6,32 @@ export default function Pagination({ metadata }: { metadata: ListMetadata }) {
     const pageNumbers = Array.from({ length: metadata.totalPages }, (_, index) => index + 1);
     return (
         <>
-            <Flex gap={ 2 }>
+            <Box m={ 10 }>
+                <Flex gap={ 2 } justify="center">
 
-                { metadata.page > 1 && (
-                    <Link to={ `/pets/?p=${ metadata.page - 1 }` }>
-                        <IconButton aria-label="Previous Page" icon={ <ArrowBackIcon/> } mr={ 2 }/>
-                    </Link>
-                ) }
+                    { metadata.page > 1 && (
+                        <Link to={ `/pets/?p=${ metadata.page - 1 }` }>
+                            <IconButton aria-label="Previous Page" icon={ <ArrowBackIcon/> } mr={ 2 }/>
+                        </Link>
+                    ) }
 
-                { pageNumbers.map((no) => {
-                        const isFirstPage = no === 1;
-                        const isCurrentPage = no === metadata.page;
-                        return (
-                            <Link key={ no } to={ isFirstPage ? "/pets" : `/pets/?p=${ no }` }
-                                  className={ isCurrentPage ? "disabled" : "" }><Button>{ no }</Button></Link>
-                        )
-                    }
-                ) }
+                    { pageNumbers.map((no) => {
+                            const isFirstPage = no === 1;
+                            const isCurrentPage = no === metadata.page;
+                            return (
+                                <Link key={ no } to={ isFirstPage ? "/pets" : `/pets/?p=${ no }` }
+                                      className={ isCurrentPage ? "disabled" : "" }><Button>{ no }</Button></Link>
+                            )
+                        }
+                    ) }
 
-                { metadata.page < metadata.totalPages && (
-                    <Link to={ `/pets/?p=${ metadata.page + 1 }` }>
-                        <IconButton aria-label="Next Page" icon={ <ArrowForwardIcon/> } ml={ 2 }/>
-                    </Link>
-                ) }
+                    { metadata.page < metadata.totalPages && (
+                        <Link to={ `/pets/?p=${ metadata.page + 1 }` }>
+                            <IconButton aria-label="Next Page" icon={ <ArrowForwardIcon/> } ml={ 2 }/>
+                        </Link>
+                    ) }
 
-            </Flex>
+                </Flex></Box>
         </>
     );
 }
