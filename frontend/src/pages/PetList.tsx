c152f1916858f51479e517c0051f97af337d2810
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-05-28 16:07:46 +0530

    use env variables to pull the table names.

--- frontend/src/pages/PetList.tsx
+++ frontend/src/pages/PetList.tsx
@@ -1,14 +1,14 @@
 import pb from "lib/pocketbase.ts";
 
-import {useEffect, useState} from "react";
-import {Pet as PetInterface} from "../models/Pet.ts";
+import { useEffect, useState } from "react";
+import { Pet as PetInterface } from "../models/Pet.ts";
 import PetComponent from "../components/PetComponent.tsx";
 
 export default function PetList() {
     const [pets, setPets] = useState<PetInterface[]>([]);
 
     async function getPets() {
-        const resultList = await pb.collection('pets').getList(1, 10);
+        const resultList = await pb.collection(import.meta.env.VITE_PB_PET_TABLE).getList(1, 10);
 
         const petList = resultList.items.map(function (pet) {
             // @ts-ignore
--- frontend/src/pages/PetPage.tsx
+++ frontend/src/pages/PetPage.tsx
@@ -1,7 +1,7 @@
 import pb from "lib/pocketbase.ts";
 
-import {useEffect, useState} from "react";
-import {Pet as PetInterface} from "../models/Pet.ts";
+import { useEffect, useState } from "react";
+import { Pet as PetInterface } from "../models/Pet.ts";
 import PetComponent from "../components/PetComponent.tsx";
 
 export default function PetPage() {
@@ -17,7 +17,7 @@ export default function PetPage() {
     });
 
     async function getPet() {
-        const record = await pb.collection('pets').getOne('l2ov1h6ju1itl48');
+        const record = await pb.collection(import.meta.env.VITE_PB_PET_TABLE).getOne('l2ov1h6ju1itl48');
         // @ts-ignore
         const pet = record as PetInterface;
         console.log(pet);
