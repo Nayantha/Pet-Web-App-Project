dfb6da71e452b9d36bc923c8b7d14a18e9c91633
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-05-29 17:36:40 +0530

    rearrange the components.

--- frontend/src/components/PetComponent.tsx
+++ frontend/src/components/PetComponent.tsx
@@ -1,15 +1,44 @@
-import {Pet as PetInterface} from "../models/Pet.ts";
-import {Card, CardBody, CardFooter, CardHeader, Text} from '@chakra-ui/react'
+import { Pet as PetInterface } from "../models/Pet.ts";
+import { Box, Button, Card, CardBody, CardFooter, CardHeader, Flex, Spacer, Text } from '@chakra-ui/react'
 
 export default function PetComponent({pet}: { pet: PetInterface }) {
     return (
         <>
             <Card>
                 <CardHeader>
-                    {pet.baseColor}
+                    { pet.name }
                 </CardHeader>
                 <CardBody>
-                    <Text>View a summary of all your customers over the last month.</Text>
+                    <Flex justify="center" align="stretch">
+                        <Box w="250px">
+                            <Flex>
+                                <Text>intake reason : </Text>
+                                <Spacer/>
+                                <Text>{ pet.intakeReason }</Text>
+                            </Flex>
+                            <Flex>
+                                <Text>breed : </Text>
+                                <Spacer/>
+                                <Text>{ pet.breed }</Text>
+                            </Flex>
+                            <Flex>
+                                <Text>species : </Text>
+                                <Spacer/>
+                                <Text>{ pet.species }</Text>
+                            </Flex>
+                            <Flex>
+                                <Text>gender : </Text>
+                                <Spacer/>
+                                <Text>{ pet.gender }</Text>
+                            </Flex>
+                            <Flex>
+                                <Text>base color : </Text>
+                                <Spacer/>
+                                <Text>{ pet.baseColor }</Text>
+                            </Flex>
+                            { !pet.adopted && <Button>Adopt</Button> }
+                        </Box>
+                    </Flex>
                 </CardBody>
                 <CardFooter></CardFooter>
             </Card>
