cb48bf4e4e5d76c9c48884c035cf7696272b4353
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-05-29 19:03:11 +0530

    create a new hook to execute the adopt functionality.

---
+++ frontend/src/hooks/useAdopt.ts
@@ -0,0 +1,30 @@
+import pb from "../lib/pocketbase.ts";
+import { useMutation } from "react-query";
+
+export default function useAdopt() {
+    async function adoptPet(adoptData: AdoptionData) {
+        const adoptedState = await pb.collection(import.meta.env.VITE_PB_PET_TABLE).getOne(adoptData.petId, {
+            fields: 'adopted',
+        });
+
+        const resultList = await pb.collection(import.meta.env.VITE_PB_ADOPTION_TABLE).getFullList({
+            filter: 'pet = "' + adoptData.petId + '"',
+        });
+
+        if (resultList.length > 0) {
+            throw new Error('Pet is already adopted by another user');
+        }
+
+        if (!adoptedState.adopted && resultList.length === 0) {
+            // this conversion is required because the keywords used make sense in the database layer
+            const data = {
+                pet: adoptData.petId,
+                user: adoptData.userId
+            }
+            await pb.collection(import.meta.env.VITE_PB_ADOPTION_TABLE).create(data);
+        }
+
+    }
+
+    return useMutation(adoptPet);
+}
