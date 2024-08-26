fbb96a699540338fc700ea0ccde29bec3b700274
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-08-26 18:18:55 +0530

    fill with some components.

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,36 @@ import "../assets/Footer.css";
 const Footer = () => {
     return (
         <footer>
-            this is the footer
+            <div className="links">
+                <strong><a href="/">Pet Shop</a> </strong>
+                <div className="social-links">
+                    <strong>Contact</strong>
+                    <a className="social-link" href="#" target="_blank">Instagram</a>
+                    <a className="social-link" href="#" target="_blank">Facebook</a>
+                    <a className="social-link" href="#" target="_blank">Pinterest</a>
+                    <a className="social-link" href="#" target="_blank">YouTube</a>
+                </div>
+                <div className="resource-links">
+                    <strong>Resources</strong>
+                    <a className="resource-link" href="#" target="_blank">Return Policy</a>
+                    <a className="resource-link" href="#" target="_blank">Track Your Order</a>
+                    <a className="resource-link" href="#" target="_blank">FAQs</a>
+                    <a className="resource-link" href="#" target="_blank">Privacy Policy</a>
+                </div>
+                <div className="about-links">
+                    <strong>Contact</strong>
+                    <a className="about-link" href="#" target="_blank">Our Mission</a>
+                    <a className="about-link" href="#" target="_blank">Join Our Community</a>
+                    <a className="about-link" href="#" target="_blank">Press Release</a>
+                </div>
+            </div>
+            <div className="copyright-text">
+                &copy; 2024
+                <a href="https://github.com/Nayantha">014 - Nayantha Koralegama</a>,
+                <a href="https://github.com/NimeshaKavindi">021 - Nimesha Kavindi</a>,
+                <a href="https://github.com/PiyumalT">035 - Tharindu Piyumal</a>,
+                <a href="https://github.com/desilva-se19047">047 - Achini Prasadini</a>
+            </div>
         </footer>
     );
 };
