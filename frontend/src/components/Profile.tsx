3e11c8d1db271d4dfbe01e487488888bcd0b0930
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-01 21:14:17 +0530

    re-organize and improve from DRY.

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -11,72 +11,75 @@ import {
     DrawerOverlay,
     useDisclosure
 } from '@chakra-ui/react'
-import { useRef } from "react";
+import { ReactNode, useRef } from "react";
 import useLogout from "../hooks/useLogout.ts";
 
 function Profile() {
     const logout = useLogout();
     const isLoggedIn = pb.authStore.isValid;
     const userModel: RecordModel = pb.authStore.model as RecordModel;
-    const { isOpen, onOpen, onClose } = useDisclosure()
+    const {isOpen, onOpen, onClose} = useDisclosure();
     const btnRef = useRef<HTMLButtonElement | null>(null);
-    if ( isLoggedIn )
+
+    const DrawerButton = () => (
+        <Button ref={ btnRef } colorScheme='teal' onClick={ onOpen }>
+            Open
+        </Button>
+    );
+
+    const DrawerComponent = ({children}: { children: ReactNode }) => (
+        <Drawer
+            isOpen={ isOpen }
+            placement='right'
+            onClose={ onClose }
+            finalFocusRef={ btnRef }
+        >
+            <DrawerOverlay/>
+            <DrawerContent>
+                { children }
+            </DrawerContent>
+        </Drawer>
+    );
+
+    if (isLoggedIn)
         return (
             <>
-                <Button ref={btnRef} colorScheme='teal' onClick={onOpen}>
-                    Open
-                </Button>
-                <Drawer
-                    isOpen={isOpen}
-                    placement='right'
-                    onClose={onClose}
-                    finalFocusRef={btnRef}
-                >
-                    <DrawerOverlay/>
-                    <DrawerContent>
-                        <DrawerHeader>
-                            <img src={pb.files.getUrl( userModel, userModel.avatar )}
-                                 alt={"profile pic of user" + userModel.username}/>
-                        </DrawerHeader>
-                        <DrawerBody>
-                            <div>Email : {userModel?.email}</div>
-                            <div>Name : {userModel?.name}</div>
-                            <div>UserName : {userModel?.username}</div>
-                        </DrawerBody>
-                        <DrawerFooter>
-                            <Button variant='outline' mr={3} onClick={onClose}>
-                                Close
-                            </Button>
-                            <Button colorScheme='blue' onClick={logout}>Logout</Button>
-                        </DrawerFooter>
-                    </DrawerContent>
-                </Drawer>
+                <DrawerButton/>
+                <DrawerComponent>
+                    <DrawerHeader>
+                        <img src={ pb.files.getUrl(userModel, userModel.avatar) }
+                             alt={ "profile pic of user" + userModel.username }/>
+                    </DrawerHeader>
+                    <DrawerBody>
+                        <div>Email : { userModel?.email }</div>
+                        <div>Name : { userModel?.name }</div>
+                        <div>UserName : { userModel?.username }</div>
+                    </DrawerBody>
+                    <DrawerFooter bg='transparent'>
+                        <Button variant='outline' mr={ 3 } onClick={ onClose }>
+                            Close
+                        </Button>
+                        <Button colorScheme='blue' onClick={ logout }>Logout</Button>
+                    </DrawerFooter>
+                </DrawerComponent>
             </>
         )
 
     return (
         <>
-            <Button ref={btnRef} colorScheme='teal' onClick={onOpen}>
+            <Button ref={ btnRef } colorScheme='teal' onClick={ onOpen }>
                 Open
             </Button>
-            <Drawer
-                isOpen={isOpen}
-                placement='right'
-                onClose={onClose}
-                finalFocusRef={btnRef}
-            >
-                <DrawerOverlay/>
-                <DrawerContent>
-                    <DrawerHeader>
-                        Login or Register
-                    </DrawerHeader>
-                    <DrawerBody>
-                        <Link to="/login">Login</Link>
-                        <br/>
-                        <Link to="/register">Register</Link>
-                    </DrawerBody>
-                </DrawerContent>
-            </Drawer>
+            <DrawerComponent>
+                <DrawerHeader>
+                    Login or Register
+                </DrawerHeader>
+                <DrawerBody>
+                    <Link to="/login">Login</Link>
+                    <br/>
+                    <Link to="/register">Register</Link>
+                </DrawerBody>
+            </DrawerComponent>
         </>
     )
 }
