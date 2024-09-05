e644885e352873f847ff3867e201aeac7e438c07
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-05 18:04:13 +0530

    add adopted data interface.

---
+++ frontend/src/models/AdoptedData.ts
@@ -0,0 +1,9 @@
+// this interface holds data / record from pet adoption table
+interface AdoptedData {
+    id: string;
+    pet: string;
+    updated: Date;
+    created: Date;
+    user: string;
+    verified: boolean;
+}
