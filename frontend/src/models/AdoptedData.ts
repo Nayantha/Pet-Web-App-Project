3714359dfee8714107a6f344917b1e9f89e929f2
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-05 18:53:54 +0530

    change the type of the user field.

--- frontend/src/models/AdoptedData.ts
+++ frontend/src/models/AdoptedData.ts
@@ -1,11 +1,12 @@
 // this interface holds data / record from pet adoption table
 import { Pet } from "./Pet.ts";
+import User from "./User.ts";
 
 export default interface AdoptedData {
     id: string;
     pet: Pet;
     updated: Date;
     created: Date;
-    user: string;
+    user: User;
     verified: boolean;
 }
