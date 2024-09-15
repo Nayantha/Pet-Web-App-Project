5a60de960d1716f7c07105fdc03f25951bf7cda5
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-15 22:46:50 +0530

    update backend.

---
+++ backend/pb_migrations/1726416161_updated_pets.js
@@ -0,0 +1,259 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("pw3zzr4jyyk1uz3")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "6n2849ob",
+    "name": "avatar",
+    "type": "file",
+    "required": true,
+    "presentable": true,
+    "unique": false,
+    "options": {
+      "mimeTypes": [
+        "image/png",
+        "image/jpeg",
+        "image/webp"
+      ],
+      "thumbs": [],
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "29rvhscr",
+    "name": "intakeReason",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "tmwdoqmp",
+    "name": "intakeDate",
+    "type": "date",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": "",
+      "max": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "p8fm18ss",
+    "name": "breed",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "yqs5irgv",
+    "name": "baseColor",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ysb6wmaz",
+    "name": "species",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "abgrbfxl",
+    "name": "gender",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "mop3x41t",
+    "name": "name",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("pw3zzr4jyyk1uz3")
+
+  // remove
+  collection.schema.removeField("6n2849ob")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "29rvhscr",
+    "name": "intakeReason",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "tmwdoqmp",
+    "name": "intakeDate",
+    "type": "date",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": "",
+      "max": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "p8fm18ss",
+    "name": "breed",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "yqs5irgv",
+    "name": "baseColor",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ysb6wmaz",
+    "name": "species",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "abgrbfxl",
+    "name": "gender",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "mop3x41t",
+    "name": "name",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
