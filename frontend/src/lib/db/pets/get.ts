b96e4f5f2e561531cb6ee37afd6b2a3185acac7c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-15 20:32:51 +0530

    use the request queries with the db data fetching.

--- frontend/src/lib/db/pets/get.ts
+++ frontend/src/lib/db/pets/get.ts
@@ -1,10 +1,11 @@
 import pb from "lib/pocketbase.ts";
+import PetRequestQuery from "models/RequestQuery/PetRequestQuery.ts";
 
-export default async function get(page: number, species: string) {
+export default async function get(page: number, petRequestQuery: PetRequestQuery) {
     try {
         return await pb.collection(import.meta.env.VITE_PB_PET_TABLE)
             .getList(page, import.meta.env.VITE_PB_PET_LIST_SIZE, {
-                filter: `adopted = False && species ~ '${ species }'`
+                filter: petRequestQuery.convertToQueryString()
             });
     } catch (error) {
         console.error('Error fetching pet list:', error);
