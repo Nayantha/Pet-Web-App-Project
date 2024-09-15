aecb159ff2f694a855db0ffd7d41d5ff2c8f4b87
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-15 20:42:27 +0530

    add a new function to add a new field to request query.

--- frontend/src/models/RequestQuery/PetRequestQuery.ts
+++ frontend/src/models/RequestQuery/PetRequestQuery.ts
@@ -4,4 +4,8 @@ export default class PetRequestQuery extends BaseRequestQuery {
     constructor(fields?: { [key: string]: { value: any, operator: string } }) {
         super(fields);
     }
+
+    public addNewField(fieldName: string, value: any, operator: string): void {
+        this.addField(fieldName, value, operator);
+    }
 }
