ebc7e239f24a6101c299e0334f5c51f4558783d4
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-15 22:03:04 +0530

    add a new function to convert pb record model to pet.

--- frontend/src/lib/petConverters.ts
+++ frontend/src/lib/petConverters.ts
@@ -1,13 +1,19 @@
 import Pet from "models/Pet.ts";
 import { ListResult, RecordModel } from "pocketbase";
+import pb from "./pocketbase.ts";
 
 export function extractPetListAndListMetadata(resultList: ListResult<RecordModel>) {
     const listMetadata = resultList as ListMetadata;
 
     const petList = resultList.items.map(function (pet) {
-        // @ts-ignore
-        return pet as Pet;
+        return recordModelToPet(pet);
     })
 
     return { petList, listMetadata }
+}
+
+export function recordModelToPet(recordModel: RecordModel) {
+    const pet = recordModel as unknown as Pet;
+    pet.avatar = pb.files.getUrl(recordModel, recordModel.avatar);
+    return pet;
 }
