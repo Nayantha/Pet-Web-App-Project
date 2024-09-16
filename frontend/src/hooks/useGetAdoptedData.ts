0613ad3e483d173916ccd7e53fcf227b06d9374b
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-16 18:23:25 +0530

    add a new hook query to fetch pet adoption data.

---
+++ frontend/src/hooks/useGetAdoptedData.ts
@@ -0,0 +1,22 @@
+import PetRequestQuery from "../models/RequestQuery/PocketBaseRequestQuery.ts";
+import RequestQuery from "../models/RequestQuery/PocketBaseRequestQuery.ts";
+import { db } from "../lib/db.ts";
+import { ComparisonOperators } from "../models/RequestQuery/ComparisonOperators.ts";
+import { useQuery } from "react-query";
+
+export default function (adoptionData: AdoptionData) {
+    return useQuery([`pet-${ adoptionData.petId }`], async () => {
+        const pet = await db.pet.get(adoptionData.petId, new PetRequestQuery({}));
+
+        const adoptionRequestQuery = new RequestQuery({
+            fields: {
+                pet: { value: adoptionData.petId, operator: ComparisonOperators.Equal },
+                user: { value: adoptionData.userId, operator: ComparisonOperators.Equal }
+            }
+        });
+
+        const adoptedData = await db.adopt.get(adoptionRequestQuery);
+        adoptedData.pet = pet;
+        return adoptedData;
+    });
+}
