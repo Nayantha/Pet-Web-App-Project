4b883134de71eef0349f200c50af632d4199f25e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-16 15:48:49 +0530

    use pocket base request query class.

--- frontend/src/hooks/usePetSpecies.ts
+++ frontend/src/hooks/usePetSpecies.ts
@@ -3,8 +3,8 @@ import { getCurrentPageNumberFromQueryParameters } from "utils/ListPages.ts";
 import { useParams } from "react-router-dom";
 import { db } from "lib/db.ts";
 import { extractPetListAndListMetadata } from "lib/petConverters.ts";
-import PetRequestQuery from "models/RequestQuery/PetRequestQuery.ts";
-import { ComparisonOperators } from "../models/RequestQuery/ComparisonOperators.ts";
+import { ComparisonOperators } from "models/RequestQuery/ComparisonOperators.ts";
+import PocketBaseRequestQuery from "models/RequestQuery/PocketBaseRequestQuery.ts";
 
 export default function usePetSpecies() {
     // Do not initialize metadata instance which will lead to a not data fetching in the hook when page changes
@@ -12,11 +12,15 @@ export default function usePetSpecies() {
     const page = getCurrentPageNumberFromQueryParameters({});
     const { species } = useParams();
     const petSpecies = String(species);
-    const petRequestQuery = new PetRequestQuery({
-        adopted: { value: false, operator: ComparisonOperators.Equal },
-        species: { value: petSpecies, operator: ComparisonOperators.Like_OR_Contains }
+    const petRequestQuery = new PocketBaseRequestQuery({
+        fields: {
+            adopted: { value: false, operator: ComparisonOperators.Equal },
+            species: { value: petSpecies, operator: ComparisonOperators.Like_OR_Contains }
+        },
+        page: page,
+        sort: "-created"
     });
     return useQuery([`pets-${ species }`, page], async () => {
-        return extractPetListAndListMetadata(await db.pets.get(page, petRequestQuery))
+        return extractPetListAndListMetadata(await db.pets.get(petRequestQuery))
     });
 }
