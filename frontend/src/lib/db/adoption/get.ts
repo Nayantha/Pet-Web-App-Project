ac0cec690f9c86fb83e4f152160f6b8dbac6745a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-16 18:43:14 +0530

    add two functions to get adoption records.

--- frontend/src/lib/db/adoption/get.ts
+++ frontend/src/lib/db/adoption/get.ts
@@ -2,6 +2,10 @@ import AdoptedData from "models/AdoptedData.ts";
 import PocketBaseRequestQuery from "models/RequestQuery/PocketBaseRequestQuery.ts";
 import pb from "lib/pocketbase.ts";
 
-export default async function (adoptRequestQuery: PocketBaseRequestQuery) {
+export async function getFirstOfList(adoptRequestQuery: PocketBaseRequestQuery) {
     return await pb.collection(import.meta.env.VITE_PB_ADOPTION_TABLE).getFirstListItem(adoptRequestQuery.convertToQueryString()) as AdoptedData;
+}
+
+export async function getFullList(adoptRequestQuery: PocketBaseRequestQuery) {
+    return await pb.collection(import.meta.env.VITE_PB_ADOPTION_TABLE).getFullList(adoptRequestQuery.exportDataToRecordListOptions()) as AdoptedData[];
 }
