91a07f113d162a477b5e116bfeecab4be305cdb0
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-16 17:32:26 +0530

    remove try and catch block.

--- frontend/src/lib/db/pet/get.ts
+++ frontend/src/lib/db/pet/get.ts
@@ -3,10 +3,5 @@ import Pet from "models/Pet.ts";
 import PocketBaseRequestQuery from "models/RequestQuery/PocketBaseRequestQuery.ts";
 
 export default async function get(petId: string, petRequestQuery: PocketBaseRequestQuery) {
-    try {
-        return await pb.collection(import.meta.env.VITE_PB_PET_TABLE).getOne(petId, petRequestQuery.exportDataToRecordListOptions()) as Pet;
-    } catch (e: any) {
-        console.error(e.message);
-        throw Error(e.message);
-    }
+    return await pb.collection(import.meta.env.VITE_PB_PET_TABLE).getOne(petId, petRequestQuery.exportDataToRecordListOptions()) as Pet;
 }
