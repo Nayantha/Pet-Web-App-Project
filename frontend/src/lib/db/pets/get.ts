2ee0f1e18887f2f54a471420361436eb0af0abe2
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-16 15:31:28 +0530

    reformat the code to use variables from request query parameter object.

--- frontend/src/lib/db/pets/get.ts
+++ frontend/src/lib/db/pets/get.ts
@@ -1,12 +1,10 @@
 import pb from "lib/pocketbase.ts";
 import PetRequestQuery from "models/RequestQuery/PetRequestQuery.ts";
 
-export default async function get(page: number, petRequestQuery: PetRequestQuery) {
+export default async function get(petRequestQuery: PetRequestQuery) {
     try {
         return await pb.collection(import.meta.env.VITE_PB_PET_TABLE)
-            .getList(page, import.meta.env.VITE_PB_PET_LIST_SIZE, {
-                filter: petRequestQuery.convertToQueryString()
-            });
+            .getList(petRequestQuery.page, petRequestQuery.perPage, petRequestQuery.exportDataToRecordListOptions());
     } catch (error) {
         console.error('Error fetching pet list:', error);
         throw error;
