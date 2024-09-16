567a428953ec5ae3c0e2c9c083cf6fb2fdb4e766
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-16 18:44:36 +0530

    extract adoption table name to a constant.

--- frontend/src/lib/db/adoption/get.ts
+++ frontend/src/lib/db/adoption/get.ts
@@ -2,10 +2,12 @@ import AdoptedData from "models/AdoptedData.ts";
 import PocketBaseRequestQuery from "models/RequestQuery/PocketBaseRequestQuery.ts";
 import pb from "lib/pocketbase.ts";
 
+const ADOPTION_TABLE = import.meta.env.VITE_PB_ADOPTION_TABLE;
+
 export async function getFirstOfList(adoptRequestQuery: PocketBaseRequestQuery) {
-    return await pb.collection(import.meta.env.VITE_PB_ADOPTION_TABLE).getFirstListItem(adoptRequestQuery.convertToQueryString()) as AdoptedData;
+    return await pb.collection(ADOPTION_TABLE).getFirstListItem(adoptRequestQuery.convertToQueryString()) as AdoptedData;
 }
 
 export async function getFullList(adoptRequestQuery: PocketBaseRequestQuery) {
-    return await pb.collection(import.meta.env.VITE_PB_ADOPTION_TABLE).getFullList(adoptRequestQuery.exportDataToRecordListOptions()) as AdoptedData[];
+    return await pb.collection(ADOPTION_TABLE).getFullList(adoptRequestQuery.exportDataToRecordListOptions()) as AdoptedData[];
 }
