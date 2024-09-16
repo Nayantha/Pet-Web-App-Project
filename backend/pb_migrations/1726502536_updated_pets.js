9e74ba9916efec35ae3b1d0c7cdb7deb0f7cb39c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-16 22:04:44 +0530

    update the backend.

---
+++ backend/pb_migrations/1726502536_updated_pets.js
@@ -0,0 +1,56 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+    const dao = new Dao(db)
+    const collection = dao.findCollectionByNameOrId("pw3zzr4jyyk1uz3")
+
+    // update
+    collection.schema.addField(new SchemaField({
+        "system": false,
+        "id": "6n2849ob",
+        "name": "avatar",
+        "type": "file",
+        "required": true,
+        "presentable": true,
+        "unique": false,
+        "options": {
+            "mimeTypes": [
+                "image/png",
+                "image/jpeg",
+                "image/webp"
+            ],
+            "thumbs": [],
+            "maxSelect": 1,
+            "maxSize": 5242880,
+            "protected": false
+        }
+    }))
+
+    return dao.saveCollection(collection)
+}, (db) => {
+    const dao = new Dao(db)
+    const collection = dao.findCollectionByNameOrId("pw3zzr4jyyk1uz3")
+
+    // update
+    collection.schema.addField(new SchemaField({
+        "system": false,
+        "id": "6n2849ob",
+        "name": "avatar",
+        "type": "file",
+        "required": false,
+        "presentable": true,
+        "unique": false,
+        "options": {
+            "mimeTypes": [
+                "image/png",
+                "image/jpeg",
+                "image/webp"
+            ],
+            "thumbs": [],
+            "maxSelect": 1,
+            "maxSize": 5242880,
+            "protected": false
+        }
+    }))
+
+    return dao.saveCollection(collection)
+})
