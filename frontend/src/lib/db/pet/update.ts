a7e048271845975fa96e234333bd7c0d9ecbc882
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-16 20:12:55 +0530

    add same functions with different names.

--- frontend/src/lib/db/pet/update.ts
+++ frontend/src/lib/db/pet/update.ts
@@ -6,7 +6,19 @@ export async function updateAdoptionStateToTrue(petId: string) {
     });
 }
 
+export async function adoptionThePet(petId: string) {
+    await pb.collection(import.meta.env.VITE_PB_PET_TABLE).update(petId, {
+        adopted: true,
+    });
+}
+
 export async function updateAdoptionStateToFalse(petId: string) {
+    await pb.collection(import.meta.env.VITE_PB_PET_TABLE).update(petId, {
+        adopted: false,
+    });
+}
+
+export async function UnAdoptionThePet(petId: string) {
     await pb.collection(import.meta.env.VITE_PB_PET_TABLE).update(petId, {
         adopted: false,
     });
