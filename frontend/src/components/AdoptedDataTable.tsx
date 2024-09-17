22f837c4896f0d2ef22fabf0aeee066b2237561c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-17 17:55:25 +0530

    add component to handle UI logic.

---
+++ frontend/src/components/AdoptedDataTable.tsx
@@ -0,0 +1,64 @@
+import { Avatar, Table, TableCaption, TableContainer, Tbody, Td, Tfoot, Th, Thead, Tr } from "@chakra-ui/react";
+import Pagination from "./Pagination.tsx";
+import ExpandedAdoptedData from "../models/ExpandedAdoptedData.ts";
+import UnAdoptAlertDialog from "./UnAdoptAlertDialog.tsx";
+
+const TableHeadings = () => {
+    return (
+        <>
+            <Tr>
+                <Th>ID</Th>
+                <Th>Name</Th>
+                <Th>Avatar</Th>
+                <Th>Adopted At</Th>
+                <Th>Adoption Approval</Th>
+                <Th>Adoption Remove / Un Adopt</Th>
+            </Tr>
+        </>
+    )
+}
+
+export default function ({ data }: {
+    data: { expandedAdoptedDataList: ExpandedAdoptedData[], listMetadata: ListMetadata }
+}) {
+    console.log(data);
+    return (
+        <>
+            <TableContainer>
+                <Table variant='simple'>
+                    <TableCaption>Adopted Pets</TableCaption>
+                    <Thead>
+                        <TableHeadings/>
+                    </Thead>
+                    <Tbody>
+                        { data.expandedAdoptedDataList.map((item) => (
+                            <Tr key={ item.id }>
+                                <Td>{ item.id }</Td>
+                                <Td>{ item.pet.name }</Td>
+                                <Td>
+                                    <Avatar src={ item.pet.avatar } name={ item.pet.name }/>
+                                </Td>
+                                <Td>
+                                    { new Date(item.created).toLocaleDateString() }
+                                </Td>
+                                <Td>
+                                    { item.verified ? "Approved" : "Pending" }
+                                </Td>
+                                <Td>
+                                    <UnAdoptAlertDialog unAdoptFunction={ () => {
+                                    } }/>
+                                </Td>
+                            </Tr>
+                        )) }
+                    </Tbody>
+                    <Tfoot>
+                        <TableHeadings/>
+                    </Tfoot>
+                </Table>
+            </TableContainer>
+            <Pagination
+                metadata={ data.listMetadata }
+            />
+        </>
+    )
+}
