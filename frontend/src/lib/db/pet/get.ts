dff02693666e6bfcace511df0a0502ce656aacd7
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-17 14:34:13 +0530

    use the pre-declared pet conversion method.

--- frontend/src/lib/db/pet/get.ts
+++ frontend/src/lib/db/pet/get.ts
@@ -1,9 +1,7 @@
 import pb from "lib/pocketbase.ts";
-import Pet from "models/Pet.ts";
 import PocketBaseRequestQuery from "models/RequestQuery/PocketBaseRequestQuery.ts";
+import { recordModelToPet } from "lib/petConverters.ts";
 
 export default async function get(petId: string, petRequestQuery: PocketBaseRequestQuery) {
-    const pet = await pb.collection(import.meta.env.VITE_PB_PET_TABLE).getOne(petId, petRequestQuery.exportDataToRecordListOptions()) as Pet;
-    pet.avatar = pb.files.getUrl(pet, pet.avatar);
-    return pet;
+    return recordModelToPet(await pb.collection(import.meta.env.VITE_PB_PET_TABLE).getOne(petId, petRequestQuery.exportDataToRecordListOptions()));
 }
