ed97533d46da844aa4591c009ba08f54b5f9d890
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-17 18:38:41 +0530

    add new component to display alert dialogs.

---
+++ frontend/src/components/AlertDialog.tsx
@@ -0,0 +1,37 @@
+import { Alert, AlertDescription, AlertIcon, AlertTitle, Box, CloseButton, useDisclosure } from "@chakra-ui/react";
+
+enum AlertStatus {
+    INFO = "info",
+    WARNING = "warning",
+    SUCCESS = "success",
+    ERROR = "error",
+    LOADING = "loading"
+}
+
+interface AlertDialog {
+    alertStatus: AlertStatus;
+    alertTitle: string;
+    alertMessage: string;
+}
+
+export default function ({ alertStatus, alertTitle, alertMessage }: AlertDialog) {
+    const { onClose } = useDisclosure({ defaultIsOpen: false });
+    return (
+        <Alert status={ alertStatus }>
+            <AlertIcon/>
+            <Box>
+                <AlertTitle>{ alertTitle }</AlertTitle>
+                <AlertDescription>
+                    { alertMessage }
+                </AlertDescription>
+            </Box>
+            <CloseButton
+                alignSelf='flex-start'
+                position='relative'
+                right={ -1 }
+                top={ -1 }
+                onClick={ onClose }
+            />
+        </Alert>
+    )
+}
