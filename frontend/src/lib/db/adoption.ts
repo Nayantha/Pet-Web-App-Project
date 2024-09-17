fc0123c98c1dd728416f47dc9cbca0f39d7b7686
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-17 18:01:00 +0530

    expose the get paginated list function.

--- frontend/src/lib/db/adoption.ts
+++ frontend/src/lib/db/adoption.ts
@@ -1,5 +1,5 @@
 import post from "./adoption/post.ts";
-import { getFirstOfList, getFullList } from "./adoption/get.ts";
+import { getFirstOfList, getFullList, getList } from "./adoption/get.ts";
 import erase from "./adoption/erase.ts";
 
 
@@ -7,5 +7,6 @@ export const adoption = {
     post,
     getFirstOfList,
     getFullList,
+    getList,
     erase
 };
