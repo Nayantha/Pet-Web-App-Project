9865a0a7053a4f6fe5a57faa0180b058e58a92c9
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-18 12:03:20 +0530

    add request query parameter object to post request.

--- frontend/src/lib/db/adoption/post.ts
+++ frontend/src/lib/db/adoption/post.ts
@@ -1,5 +1,7 @@
 import pb from "lib/pocketbase.ts";
+import PocketBaseRequestQuery from "models/RequestQuery/PocketBaseRequestQuery.ts";
+import {recordModelToAdoptedData} from "lib/adoptionConverters.ts";
 
-export default async function post(adoptData: AdoptionData) {
-    return await pb.collection(import.meta.env.VITE_PB_ADOPTION_TABLE).create(adoptData);
+export default async function post(adoptionData: AdoptionData, adoptRequestQuery: PocketBaseRequestQuery) {
+    return recordModelToAdoptedData(await pb.collection(import.meta.env.VITE_PB_ADOPTION_TABLE).create(adoptionData, adoptRequestQuery.exportDataToRecordListOptions()));
 }
