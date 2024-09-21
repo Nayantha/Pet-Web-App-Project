1b207b1aa33e9bbdadbeb34a1973d7d4fb267b52
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-21 11:50:40 +0530

    add custom login command.

--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -1,1 +1,8 @@
 /// <reference types="cypress" />
+// @ts-ignore
+Cypress.Commands.add('login', () => {
+    cy.visit('/login');
+    cy.get('input#email').type(Cypress.env('email'), { log: false });
+    cy.get('input#password').type(Cypress.env('password'), { log: false });
+    cy.get('button[type="submit"]').click();
+});
