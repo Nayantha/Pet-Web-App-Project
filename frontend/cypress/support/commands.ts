c5bbecaab39bb60d8a8b355de36e480426450684
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-21 12:36:16 +0530

    add logout command.

--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -6,3 +6,9 @@ Cypress.Commands.add('login', () => {
     cy.get('input#password').type(Cypress.env('password'), { log: false });
     cy.get('button[type="submit"]').click();
 });
+
+// @ts-ignore
+Cypress.Commands.add('logout', () => {
+    cy.get("button.profile-btn.drawer-btn").should('have.text', 'Profile').click();
+    cy.get('button.logout-btn').click();
+});
