f1c143ae40b6d227c9abc5be1d8ac1b0becc58ec
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-22 13:59:59 +0530

    use the new user interface object.

--- frontend/src/hooks/useAdoptionList.ts
+++ frontend/src/hooks/useAdoptionList.ts
@@ -1,15 +1,15 @@
 import PocketBaseRequestQuery from "models/RequestQuery/PocketBaseRequestQuery.ts";
 import { getCurrentPageNumberFromQueryParameters } from "../utils/ListPages.ts";
 import { ComparisonOperators } from "models/RequestQuery/ComparisonOperators.ts";
-import pb from "lib/pocketbase.ts";
 import { useQuery } from "react-query";
 import { db } from "lib/db.ts";
 import { extractAdoptedDataListAndListMetadata } from "lib/adoptionConverters.ts";
+import AuthenticatedUser from "lib/userStore.ts";
 
 export default function () {
     // Do not initialize metadata instance which will lead to a not data fetching in the hook when page changes
     const page = getCurrentPageNumberFromQueryParameters({} as ListMetadata);
-    const user = pb.authStore.model?.id ?? "";
+    const user = AuthenticatedUser.id;
     const adoptionRequestQuery = new PocketBaseRequestQuery({
         fields: { user: { value: user, operator: ComparisonOperators.Equal } },
         page: page,
