d580c5b13b1ebd7425c330bab22cf4a26aa5bfc9
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-22 14:01:47 +0530

    convert the relative avatar to the absolute path.

--- frontend/src/lib/userStore.ts
+++ frontend/src/lib/userStore.ts
@@ -1,5 +1,6 @@
 import pb from "./pocketbase.ts";
-import User from "../models/User.ts";
+import User from "models/User.ts";
 
 const AuthenticatedUser = pb.authStore.model as User;
+AuthenticatedUser.avatar = pb.files.getUrl(AuthenticatedUser, AuthenticatedUser.avatar);
 export default AuthenticatedUser;
