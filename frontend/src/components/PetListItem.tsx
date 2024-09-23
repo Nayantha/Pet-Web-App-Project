17d635fd92e36c0affd0c20a02c93c0b9193a601
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-23 19:59:18 +0530

    add class to the card component.

--- frontend/src/components/PetListItem.tsx
+++ frontend/src/components/PetListItem.tsx
@@ -1,40 +1,28 @@
 import Pet from "../models/Pet.ts";
-import {
-    Box,
-    Button,
-    Card,
-    CardBody,
-    CardFooter,
-    CardHeader,
-    Flex,
-    Heading,
-    Image,
-    Link,
-    Text
-} from '@chakra-ui/react';
-import { Link as RouterLink } from 'react-router-dom';
+import {Box, Button, Card, CardBody, CardFooter, CardHeader, Flex, Heading, Image, Link, Text} from '@chakra-ui/react';
+import {Link as RouterLink} from 'react-router-dom';
 
-export default function PetListItem({ pet }: { pet: Pet }) {
+export default function PetListItem({pet}: { pet: Pet }) {
     return (
         <>
-            <Card align="center" justify="center" size="sm">
+            <Card align="center" justify="center" size="sm" className="pet-list-item">
                 <CardHeader>
-                    <Heading size={ 'sm' }>{ pet.name }</Heading>
+                    <Heading size={'sm'}>{pet.name}</Heading>
                 </CardHeader>
                 <CardBody>
                     <Image
-                        src={ pet.avatar }
-                        alt={ pet.name }
+                        src={pet.avatar}
+                        alt={pet.name}
                         borderRadius='lg'
                     />
                 </CardBody>
                 <CardFooter>
                     <Flex justify="center" align="center" direction="row" gap="10">
                         <Box>
-                            <Text>{ pet.species }</Text>
-                            <Text>{ pet.breed }</Text>
+                            <Text>{pet.species}</Text>
+                            <Text>{pet.breed}</Text>
                         </Box>
-                        <Link as={ RouterLink } to={ `/pets/${ pet.id }` }>
+                        <Link as={RouterLink} to={`/pets/${pet.id}`}>
                             <Button>View Pet</Button>
                         </Link>
                     </Flex>
