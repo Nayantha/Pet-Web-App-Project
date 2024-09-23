a60c6f6603152155e47bbfeddcf7f05b52e1d974
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-23 19:05:36 +0530

    replace the test logic with custom command.

--- frontend/cypress/e2e/pet/adopt-fn.cy.ts
+++ frontend/cypress/e2e/pet/adopt-fn.cy.ts
@@ -7,22 +7,11 @@ describe("Test Adopt & Un Adopt Functions / Buttons", () => {
     })
 
     it('adopt a pet', () => {
-        cy.visit(PET_PAGE_LINK);
-
-        cy.get("button.adopt-btn").should('have.text', "Adopt");
-        cy.get("button.adopt-btn").should('not.have.attr', "disabled");
-
-        cy.get("button.adopt-btn").click();
-
-        cy.get("button.adopt-btn").should('have.attr', "disabled");
-        cy.get("button.un-adopt-btn").should('have.text', "Un Adopt Pet");
+        cy.adoptPet(PET_PAGE_LINK);
     });
 
     it('un adopt a pet', () => {
-        cy.visit(PET_PAGE_LINK);
-        cy.get("button.un-adopt-btn").click();
-        cy.get("button.un-adopt-alert-btn").click();
-        cy.get("button.adopt-btn").should('have.text', "Adopt");
+        cy.unAdoptPet(PET_PAGE_LINK);
     });
 
 })
