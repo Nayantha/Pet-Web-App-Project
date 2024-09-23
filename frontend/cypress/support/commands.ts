5c75c28518497c17fdabf5ab04c8c386cf870847
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-23 17:57:12 +0530

    extend the cypress chainable type with custom commands.

--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -1,23 +1,35 @@
 /// <reference types="cypress" />
-// @ts-ignore
 Cypress.Commands.add('login', () => {
     cy.visit('/login');
-    cy.get('input#email').type(Cypress.env('email'), { log: false });
-    cy.get('input#password').type(Cypress.env('password'), { log: false });
+    cy.get('input#email').type(Cypress.env('email'), {log: false});
+    cy.get('input#password').type(Cypress.env('password'), {log: false});
     cy.get('button[type="submit"]').click();
     cy.title().should('eq', 'PetStore');
     cy.url().should('eq', Cypress.config('baseUrl') + '/');
 });
 
-// @ts-ignore
 Cypress.Commands.add('logout', () => {
     cy.get("button.profile-btn.drawer-btn").should('have.text', 'Profile').click();
     cy.get('button.logout-btn').click();
     cy.get("a.login-btn").should('have.text', 'Login');
 });
 
-// @ts-ignore
 Cypress.Commands.add('visitAndCheckRedirect', (link: string, redirectPath: string) => {
     cy.visit(link);
     cy.url().should('include', redirectPath);
 });
+
+
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            visitAndCheckRedirect(link: string, redirectPath: string): void;
+
+            login(): void;
+
+            logout(): void;
+        }
+    }
+}
+
+export {}; // This empty export is necessary to make this a module
