f835706f52e56d0291a29c8fcd2d2be4496d7aea
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-23 17:59:30 +0530

    remove the ts ignore comments.

--- frontend/cypress/e2e/auth/logout.cy.ts
+++ frontend/cypress/e2e/auth/logout.cy.ts
@@ -1,11 +1,9 @@
 /// <reference types="cypress" />
 describe("Logout", () => {
     beforeEach("Login", () => {
-        // @ts-ignore
         cy.login();
     });
     it("Logout", () => {
-        // @ts-ignore
         cy.logout();
     });
 });
