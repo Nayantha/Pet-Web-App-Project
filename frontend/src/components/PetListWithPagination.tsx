a3e5049d1bd41278111b178a3049cb3463adefec
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-09-24 20:27:01 +0530

    modify the break point items rendering logic.

--- frontend/src/components/PetListWithPagination.tsx
+++ frontend/src/components/PetListWithPagination.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SimpleGrid} from '@chakra-ui/react';
+import { SimpleGrid } from '@chakra-ui/react';
 import Pagination from './Pagination';
 import PetListItem from './PetListItem';
 import Pet from "../models/Pet.ts";
@@ -16,11 +16,11 @@ interface PetListWithPaginationProps {
 const PetListWithPagination: React.FC<PetListWithPaginationProps> = ({ title, data }) => {
     return (
         <div className="pet-list">
-            <h1 className="title">{title}</h1>
-            <SimpleGrid columns={{base: 1, sm: 1, md: 3, lg: 4}} spacing={50} className="pet-list">
-                {data?.petList.map((pet) => (
-                    <PetListItem key={pet.id} pet={pet}/>
-                ))}
+            <h1 className="title">{ title }</h1>
+            <SimpleGrid columns={ { base: 1, md: 2, lg: 3 } } spacing={ 5 } className="pet-list">
+                { data?.petList.map((pet) => (
+                    <PetListItem key={ pet.id } pet={ pet }/>
+                )) }
             </SimpleGrid>
 
             <Pagination
