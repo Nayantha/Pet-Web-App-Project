7f9b6d9a21ad36ee09ea165389c8f6b7ae783783
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-10-04 15:12:02 +0530

    use set user in the store.

--- frontend/src/hooks/useLogin.ts
+++ frontend/src/hooks/useLogin.ts
@@ -1,13 +1,17 @@
 import { LoginAuthData } from "models/LoginAuthData.ts";
 import pb from "../lib/pocketbase.ts";
 import { useMutation } from "react-query";
+import { setUser } from "../slices/authSlice.ts";
+import { useDispatch } from "react-redux";
 
 export default function useLogin() {
+    const dispatch = useDispatch();
 
     async function login(data: LoginAuthData) {
-        await
+        const authData = await
             pb.collection(import.meta.env.VITE_PB_USER_TABLE)
                 .authWithPassword(data.email, data.password);
+        dispatch(setUser(authData.record));
         window.location.href = "/";
     }
 
