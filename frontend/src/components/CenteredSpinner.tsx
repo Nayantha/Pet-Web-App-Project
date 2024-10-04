7b7f21dc691b81cf619221c64fea8d6ca0497d36
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-10-04 12:56:59 +0530

    add a centred spinner component.

---
+++ frontend/src/components/CenteredSpinner.tsx
@@ -0,0 +1,9 @@
+import { Center, Spinner } from "@chakra-ui/react"
+
+export default function CenteredSpinner() {
+    return (
+        <Center h="100vh">
+            <Spinner/>
+        </Center>
+    )
+}
