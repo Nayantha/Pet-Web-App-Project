863cb6fd131c51ac30c1719e21ad43bca406711a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-10-04 13:11:16 +0530

    update the test case descriptions.

--- frontend/cypress/e2e/pet/adopt-fn.cy.ts
+++ frontend/cypress/e2e/pet/adopt-fn.cy.ts
@@ -2,24 +2,24 @@ describe("Test Adopt & Un Adopt Functions / Buttons", () => {
 
     const PET_PAGE_LINK = `/pets/${Cypress.env("petId")}`;
 
-    beforeEach("login", () => {
+    beforeEach("login to site", () => {
         cy.login();
     })
 
-    it('adopt a pet', () => {
+    it('able to adopt a pet', () => {
         cy.adoptPet(PET_PAGE_LINK);
     });
 
-    it('un adopt a pet', () => {
+    it('able to un adopt a pet', () => {
         cy.unAdoptPet(PET_PAGE_LINK);
     });
 
-    it("adopt and un adopt", () => {
+    it("able to adopt and then un adopt same pet", () => {
         cy.adoptPet(PET_PAGE_LINK);
         cy.unAdoptPet(PET_PAGE_LINK);
     });
 
-    it("adopt and un adopt several times", () => {
+    it("able to adopt and un adopt the same pet several times", () => {
         for (let i = 0; i < 5; i++) {
             cy.log("\n" + "iteration : " + i + "\n")
             cy.adoptPet(PET_PAGE_LINK);
