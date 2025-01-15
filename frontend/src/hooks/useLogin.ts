46b9ab81b0d6a79d16dd4a318ea4fc378a333453
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-01-15 07:24:39 +0530

    update user in auth store.

--- frontend/src/hooks/useLogin.ts
+++ frontend/src/hooks/useLogin.ts
@@ -1,17 +1,23 @@
 import { LoginAuthData } from "models/LoginAuthData.ts";
-import pb from "../lib/pocketbase.ts";
+import pb from "lib/pocketbase.ts";
 import { useMutation } from "react-query";
-import { setUser } from "../slices/authSlice.ts";
+import { updateState } from "features/auth/authSlice.ts";
 import { useDispatch } from "react-redux";
+import User from "../models/User.ts";
 
 export default function useLogin() {
     const dispatch = useDispatch();
 
     async function login(data: LoginAuthData) {
-        const authData = await
+        await
             pb.collection(import.meta.env.VITE_PB_USER_TABLE)
                 .authWithPassword(data.email, data.password);
-        dispatch(setUser(authData.record));
+        dispatch(updateState({
+            user: pb.authStore.model as unknown as User,
+            isAuthenticated: pb.authStore.isValid && pb.authStore.isAuthRecord,
+            isAdmin: pb.authStore.isAdmin,
+            token: pb.authStore.token
+        }));
         window.location.href = "/";
     }
 
