2077a1f2a79fc8cbc604372a2781c24069864b11
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-02-28 21:00:32 +0530

    expose shelters endpoint.

--- frontend/src/lib/db.ts
+++ frontend/src/lib/db.ts
@@ -1,11 +1,13 @@
 import { pets } from './db/pets.ts';
 import { pet } from "./db/pet.ts";
 import { adoption } from "./db/adoption.ts";
-import { shelter } from "./shelter.ts";
+import { shelter } from "./db/shelter.ts";
+import { shelters } from "./db/shelters.ts";
 
 export const db = {
     pets,
     pet,
     adoption,
-    shelter
+    shelter,
+    shelters,
 };
