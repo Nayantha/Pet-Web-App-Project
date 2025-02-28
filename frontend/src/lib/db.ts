d3f79fa16a2e5e3407fdf82d2a10b572e456322b
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-02-28 19:17:02 +0530

    add shelter endpoints to db.

--- frontend/src/lib/db.ts
+++ frontend/src/lib/db.ts
@@ -1,9 +1,11 @@
 import { pets } from './db/pets.ts';
 import { pet } from "./db/pet.ts";
 import { adoption } from "./db/adoption.ts";
+import { shelter } from "./shelter.ts";
 
 export const db = {
     pets,
     pet,
-    adoption
+    adoption,
+    shelter
 };
