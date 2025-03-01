a67183090d1454052694f9fd2dccc1d69ce12d80
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-01 11:46:35 +0530

    add props to te component.

--- frontend/src/components/CenteredSpinner.tsx
+++ frontend/src/components/CenteredSpinner.tsx
@@ -1,9 +1,15 @@
-import { Center, Spinner } from "@chakra-ui/react"
+import { Center, Spinner, Text } from "@chakra-ui/react"
 
-export default function CenteredSpinner() {
+interface CenteredSpinnerProps {
+    size?: string;
+    text?: string;
+}
+
+export default function CenteredSpinner({ size = "lg", text = "" }: CenteredSpinnerProps) {
     return (
         <Center h="100vh">
-            <Spinner/>
+            <Spinner size={ size }/>
+            { text && <Text mt={ 2 }>Loading { text }...</Text> }
         </Center>
     )
 }
