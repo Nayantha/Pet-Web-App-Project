3e91d85884a8bdc3bb6f458bb45bf89192e519e1
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-01 13:56:12 +0530

    remove extra data fetching logic.

--- frontend/src/lib/db/shelters/get.ts
+++ frontend/src/lib/db/shelters/get.ts
@@ -3,8 +3,6 @@ import PocketBaseRequestQuery from "models/RequestQuery/PocketBaseRequestQuery.t
 
 export default async function get(shelterRequestQuery: PocketBaseRequestQuery) {
     try {
-        const resultList = await pb.collection('shelters').getList(1, 50,);
-        console.log(resultList);
         return await pb.collection(import.meta.env.VITE_PB_SHELTER_TABLE)
             .getList(shelterRequestQuery.page, shelterRequestQuery.perPage, shelterRequestQuery.exportDataToRecordListOptions());
     } catch (error) {
