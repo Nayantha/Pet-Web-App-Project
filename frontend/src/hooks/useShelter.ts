3cc6cbeb4d185025e1a225d16654f217935dfb18
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-01 13:22:16 +0530

    fix naming error.

--- frontend/src/hooks/useShelter.ts
+++ frontend/src/hooks/useShelter.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "react-query";
 import { db } from "lib/db.ts";
 
-export default function usePet(shelterId: string) {
+export default function useShelter(shelterId: string) {
     return useQuery(['pet'], async () => {
         return await db.shelter.get(shelterId);
     });
