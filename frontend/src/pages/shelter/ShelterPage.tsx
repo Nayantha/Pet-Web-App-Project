bd2b0f53d0d8ca7bd52932b36aef502a6b5976f4
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-01 13:51:33 +0530

    render pets belong to the shelter.

--- frontend/src/pages/shelter/ShelterPage.tsx
+++ frontend/src/pages/shelter/ShelterPage.tsx
@@ -2,22 +2,55 @@ import { useLocation } from "react-router-dom";
 import useShelter from "hooks/useShelter.ts";
 import ShelterComponent from "components/ShelterComponent.tsx";
 import CenteredSpinner from "components/CenteredSpinner.tsx";
+import usePetsOfAShelter from "hooks/usePetsOfAShelter.ts";
+import ListWithPagination from "components/ListWithPagination.tsx";
+import Pet from "models/Pet.ts";
+import PetListItem from "components/PetListItem.tsx";
 
 export default function ShelterPage() {
     const location = useLocation();
     const shelterId = location.state?.id;
+    const currentPath = location.pathname;
 
-    const { data, isError, isLoading, error } = useShelter(shelterId);
+    const {
+        data: shelter,
+        isError: isFetchingShelterError,
+        isLoading: isFetchingShelterLoading,
+        error: fetchingShelterError
+    } = useShelter(shelterId);
+    const {
+        data,
+        isError: isPetsOfShelterDataFetchError,
+        isLoading: isPetsOfShelterDataFetchLoading,
+        error: petsofShelterDataFetchError
+    } = usePetsOfAShelter(shelterId);
 
-    if (isLoading) return <CenteredSpinner/>;
-    if (isError) { // @ts-ignore
-        return <div>Error: { error.message }</div>;
+    if (isFetchingShelterLoading || isPetsOfShelterDataFetchLoading) return <CenteredSpinner/>;
+    if (isFetchingShelterError) { // @ts-ignore
+        return <div>Error: { fetchingShelterError.message }</div>;
     }
 
+    if (isPetsOfShelterDataFetchError) { // @ts-ignore
+        return <div>Error: { petsofShelterDataFetchError.message }</div>;
+    }
+
+    const transformedData = {
+        items: data?.petList || [],
+        listMetadata: data?.listMetadata || {
+            totalItems: 0,
+            totalPages: 0,
+            perPage: 0,
+            page: 0,
+            baseURL: currentPath
+        }
+    };
 
     return (
         <>
-            <ShelterComponent shelter={ data ?? {} as Shelter }/>
+            <ShelterComponent shelter={ shelter ?? {} as Shelter }/>
+            <ListWithPagination data={ transformedData }
+                                renderItem={ (pet: Pet) => <PetListItem key={ pet.id } pet={ pet }/> }
+                                title={ `Pets Of Shelter ${ shelter?.name }` }/>
         </>
     )
 }
