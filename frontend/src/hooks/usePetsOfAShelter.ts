8d7e90b2ce9ac2916591fa2c72a1377ebb3c7f32
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-01 13:50:55 +0530

    add new hook to fetch pets belonf to a shelter.

---
+++ frontend/src/hooks/usePetsOfAShelter.ts
@@ -0,0 +1,18 @@
+import { useQuery } from 'react-query';
+import { getCurrentPageNumberFromQueryParameters } from "utils/ListPages.ts";
+import { extractPetListAndListMetadata } from "lib/petConverters.ts";
+import { db } from "lib/db.ts";
+import PetRequestQuery from "models/RequestQuery/PocketBaseRequestQuery.ts";
+import { ComparisonOperators } from "models/RequestQuery/ComparisonOperators.ts";
+
+export default function usePetsOfAShelter(shelter_id: string) {
+    // Do not initialize metadata instance which will lead to a not data fetching in the hook when page changes
+    const page = getCurrentPageNumberFromQueryParameters({} as ListMetadata);
+    const petRequestQuery = new PetRequestQuery({
+        fields: { shelter_id: { value: shelter_id, operator: ComparisonOperators.Equal } },
+        page: page
+    });
+    return useQuery(['pets', page], async () => {
+        return extractPetListAndListMetadata(await db.pets.get(petRequestQuery))
+    });
+}
