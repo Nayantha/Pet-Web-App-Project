d480ec18eb2863bbf5004b306a6b69bd7bead379
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-01 13:20:13 +0530

    fetch shelter and render using custom component.

--- frontend/src/pages/shelter/ShelterPage.tsx
+++ frontend/src/pages/shelter/ShelterPage.tsx
@@ -1,12 +1,23 @@
 import { useLocation } from "react-router-dom";
+import useShelter from "hooks/useShelter.ts";
+import ShelterComponent from "components/ShelterComponent.tsx";
+import CenteredSpinner from "components/CenteredSpinner.tsx";
 
 export default function ShelterPage() {
     const location = useLocation();
     const shelterId = location.state?.id;
 
+    const { data, isError, isLoading, error } = useShelter(shelterId);
+
+    if (isLoading) return <CenteredSpinner/>;
+    if (isError) { // @ts-ignore
+        return <div>Error: { error.message }</div>;
+    }
+
+
     return (
         <>
-            <div>{ shelterId }</div>
+            <ShelterComponent shelter={ data ?? {} as Shelter }/>
         </>
     )
 }
