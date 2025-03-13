ffcd1391bf2a7d41e22d7fcb353fe3d4cf5be1ed
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-13 10:47:43 +0530

    format the link strings.

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -4,13 +4,15 @@ import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
 
 export default function Pagination({ metadata }: { metadata: ListMetadata }) {
     const pageNumbers = Array.from({ length: metadata.totalPages }, (_, index) => index + 1);
+    // console.log("metadata",metadata);
+    // console.log("base URL: ",metadata.baseURL);
     return (
         <>
             <Box m={ 10 }>
                 <Flex gap={ 2 } justify="center">
 
                     { metadata.page > 1 && (
-                        <Link to={ `${ metadata.baseURL }/?p=${ metadata.page - 1 }` }>
+                        <Link to={ `${ metadata.baseURL }?p=${ metadata.page - 1 }` }>
                             <IconButton aria-label="Previous Page" icon={ <ArrowBackIcon/> } mr={ 2 }/>
                         </Link>
                     ) }
@@ -20,14 +22,14 @@ export default function Pagination({ metadata }: { metadata: ListMetadata }) {
                             const isCurrentPage = no === metadata.page;
                             return (
                                 <Link key={ no }
-                                      to={ isFirstPage ? `${ metadata.baseURL }` : `${ metadata.baseURL }/?p=${ no }` }
+                                      to={ isFirstPage ? `${ metadata.baseURL }` : `${ metadata.baseURL }?p=${ no }` }
                                       className={ isCurrentPage ? "disabled" : "" }><Button>{ no }</Button></Link>
                             )
                         }
                     ) }
 
                     { metadata.page < metadata.totalPages && (
-                        <Link to={ `/pets/?p=${ metadata.page + 1 }` }>
+                        <Link to={ `${ metadata.baseURL }?p=${ metadata.page + 1 }` }>
                             <IconButton aria-label="Next Page" icon={ <ArrowForwardIcon/> } ml={ 2 }/>
                         </Link>
                     ) }
