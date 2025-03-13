008941d5ef73eb283a28a1b2da350f4ec444e068
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-03-13 15:58:03 +0530

    build url parameters and url.

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -1,35 +1,41 @@
 import { Box, Button, Flex, IconButton } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
 
 export default function Pagination({ metadata }: { metadata: ListMetadata }) {
     const pageNumbers = Array.from({ length: metadata.totalPages }, (_, index) => index + 1);
-    // console.log("metadata",metadata);
-    // console.log("base URL: ",metadata.baseURL);
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+
+    const buildPaginationUrl = (page: number) => {
+        const newParams = new URLSearchParams(searchParams);
+        page === 1 ? newParams.delete('p') : newParams.set('p', page.toString());
+        return { pathname: location.pathname, search: newParams.toString() };
+    };
+
     return (
         <>
             <Box m={ 10 }>
                 <Flex gap={ 2 } justify="center">
 
                     { metadata.page > 1 && (
-                        <Link to={ `${ metadata.baseURL }?p=${ metadata.page - 1 }` }>
+                        <Link to={ buildPaginationUrl(metadata.page - 1) }>
                             <IconButton aria-label="Previous Page" icon={ <ArrowBackIcon/> } mr={ 2 }/>
                         </Link>
                     ) }
 
                     { pageNumbers.map((no) => {
-                            const isFirstPage = no === 1;
                             const isCurrentPage = no === metadata.page;
                             return (
                                 <Link key={ no }
-                                      to={ isFirstPage ? `${ metadata.baseURL }` : `${ metadata.baseURL }?p=${ no }` }
+                                      to={ buildPaginationUrl(no) }
                                       className={ isCurrentPage ? "disabled" : "" }><Button>{ no }</Button></Link>
                             )
                         }
                     ) }
 
                     { metadata.page < metadata.totalPages && (
-                        <Link to={ `${ metadata.baseURL }?p=${ metadata.page + 1 }` }>
+                        <Link to={ buildPaginationUrl(metadata.page + 1) }>
                             <IconButton aria-label="Next Page" icon={ <ArrowForwardIcon/> } ml={ 2 }/>
                         </Link>
                     ) }
